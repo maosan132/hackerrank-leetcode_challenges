576f7602999ff9f0cfae38300daaa4b7a66f7e1b
maosan132 <maosan132@gmail.com>
2020-07-17 17:02:37 -0500

    done. but hackerrank doesntt accept the solution for 'aaab'. with my code, it is clear that if you change index at 0, it would be a palindrome: 'baab'. Apparently hackerrank believes the only right answer is: 'aaaa'

--- palindrome_index.rb
+++ palindrome_index.rb
@@ -1,32 +1,56 @@
 require 'colorize'
 
-s = %w(aaba
-aab
-aaa
-eaxdixae)
+s = 'aaab'
 
-puts '----' * 10 + '\n' + '      MY PALIDROME INDEX SOLUTION'.yellow + '\n' + '----' * 10
-s.each do |i|
-  puts "Entering string = #{i.upcase}".red
-  if i == i.reverse
-    puts 'is a palindrome'
-    num = -1
-    puts "returns: #{num}".yellow
-  else
-    size = i.size
-    puts "steps: #{steps = size.even? ? size / 2 : (size / 2) - 1}"
-    count_deletions = 0
-    steps.times do
-      if i[0] != i[-1]
-        puts 'first and last chars are diferent!, lets equal those!'
-        i[0] = i[-1] # optional, not necessary make changes
-        break
-      else
-        i = i[1...-1]
-        puts "count_deletions = #{count_deletions += 1}"
-      end
+puts '----' * 10 + "\n" + '      MY PALIDROME INDEX SOLUTION'.yellow + "\n" + '----' * 10
+
+if s == s.reverse
+  num = -1
+  return num
+else
+  size = s.size
+  steps = size.even? ? size / 2 : (size / 2) - 1
+  count_deletions = 0
+  steps.times do
+    if s[0] != s[-1]
+      break
+    else
+      s = s[1...-1]
+      count_deletions += 1
     end
-    puts "returns index: #{count_deletions}".yellow
   end
-  puts '----' * 10
+  count_deletions
 end
+
+puts '----' * 10
+
+# s = %w(aaba
+# aab
+# aaa
+# eaxdixae)
+
+# puts '----' * 10 + '\n' + '      MY PALIDROME INDEX SOLUTION'.yellow + '\n' + '----' * 10
+# s.each do |i|
+#   puts "Entering string = #{i.upcase}".red
+#   if i == i.reverse
+#     puts 'is a palindrome'
+#     num = -1
+#     puts "returns: #{num}".yellow
+#   else
+#     size = i.size
+#     puts "steps: #{steps = size.even? ? size / 2 : (size / 2) - 1}"
+#     count_deletions = 0
+#     steps.times do
+#       if i[0] != i[-1]
+#         puts 'first and last chars are diferent!, lets equal those!'
+#         i[0] = i[-1] # optional, not necessary make changes
+#         break
+#       else
+#         i = i[1...-1]
+#         puts "count_deletions = #{count_deletions += 1}"
+#       end
+#     end
+#     puts "returns index: #{count_deletions}".yellow
+#   end
+#   puts '----' * 10
+# end
